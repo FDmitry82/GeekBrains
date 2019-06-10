355195a40d2ff2aebe7801a42dd393084f67ec3e
FDmitry <xxx_3@mail.ru>
2019-06-10 23:30:46 +0500

    Изменил последнюю задачу в Hard

--- Основы языка Python/Home_Work_Lesson04/hw04_hard.py
+++ Основы языка Python/Home_Work_Lesson04/hw04_hard.py
@@ -79,16 +79,17 @@ n = 8
 x = []
 y = []
 for i in range(n):
-    new_x, new_y = [int(s) for s in input().split()]
-    x.append(new_x)
-    y.append(new_y)
+    coords = input(f'{i} ферзь (x,y): ')
+    x.append(int(coords.split(',')[0]))
+    y.append(int(coords.split(',')[1]))
 
 correct = True
 for i in range(n):
     for j in range(i + 1, n):
         if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
             correct = False
-    if correct:
-        print('NO')
-    else:
-        print('YES')
+
+if correct:
+    print('NO')
+else:
+    print('YES')
